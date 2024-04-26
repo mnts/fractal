e03c58f2477469c612d2a3097400ab0ba175a7e7
mnts <mntask@gmail.com>
2024-04-26 22:46:23 +0700

    a

--- lib/extensions/string.dart
+++ lib/extensions/string.dart
@@ -1,8 +1,8 @@
 extension StringCasingExtension on String {
-  String toCapitalized() =>
+  String get toCapitalized =>
       length > 0 ? '${this[0].toUpperCase()}${substring(1).toLowerCase()}' : '';
-  String toTitleCase() => replaceAll(RegExp(' +'), ' ')
+  String get toTitleCase => replaceAll(RegExp(' +'), ' ')
       .split(' ')
-      .map((str) => str.toCapitalized())
+      .map((str) => str.toCapitalized)
       .join(' ');
 }
--- lib/utils.dart
+++ lib/utils.dart
@@ -1,1 +1,9 @@
 int get unixSeconds => (DateTime.now()).millisecondsSinceEpoch ~/ 1000;
+
+formatFName(String title) => title
+    .replaceAll(RegExp(r"\s+\b|\b\s"), "_")
+    .replaceAll(
+      RegExp('[^A-Za-z0-9_-]'),
+      '',
+    )
+    .toLowerCase();
